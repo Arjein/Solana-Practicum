e6d00dca9102a2a1d859471922ed49671b4c59c3
Mert Arcan <mertarcan8@outlook.com>
2023-01-03 16:52:05 +0300

    Initial Commit

---
+++ nft-staking/src/entrypoint.rs
@@ -0,0 +1,19 @@
+// Entrypoint.rs
+use solana_program::{
+    account_info::AccountInfo, entrypoint, entrypoint::ProgramResult, pubkey::Pubkey,
+};
+
+use crate::processor;
+
+entrypoint!(process_instruction);
+
+pub fn process_instruction(
+    program_id: &Pubkey,
+    accounts: &[AccountInfo],
+    instruction_data: &[u8],
+) -> ProgramResult {
+    processor::process_instruction(program_id, accounts, instruction_data)?;
+    Ok(())
+}
+
+// This macro will help process all incoming instructions
